a21db0d7fa59c362b3d3fb04f8bd6c82e78d1dad
Temir <31662@iitu.edu.kz>
2022-03-31 23:38:24 +0600

    Send Haruhi web TEST01
    тупо перенес лабки 8 Темира (то бишь моего) в папку репозитория

---
+++ src/components/Body.js
@@ -0,0 +1,48 @@
+import './Body.css'
+
+const Body = () =>{
+    return (
+        <div className='Body'>
+            <div className='newsList'>newsList
+
+                <div className='newsBlockImg1'>
+                    <div className='newsBlockText1'>今日は人類の歴史の中で最もクールなコンサートでした。 ハルヒは"神が知っている"という曲を演奏しました。.. 私は永遠にそれを見る準備ができています。</div>
+                        </div>
+
+                <div className='newsBlockImg2'>
+                    <div className='newsBlockText2'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15498回以上見てきました）</div>
+                        </div>
+
+                <div className='newsBlockImg3'>
+                    <div className='newsBlockText3'>重要なニュース！ 今日、最初の顧客はSOSチームに来ました！ （彼女はちょうど美しいです、ちょうど見て!）</div>
+                        </div>
+
+                <div className='newsBlockImg4'>
+                    <div className='newsBlockText4'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15497回以上見てきました）</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15496回以上見てきました）</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15495回以上見てきました）</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15494回以上見てきました）</div>
+                        </div>
+
+                <div className='newsBlockImg5'>
+                    <div className='newsBlockText5'>今日は最も"再現できない"祭りでした。 （私はこの祭りを15493回以上見てきました）</div>
+                        </div>
+                            </div>
+
+            <div className='newsTools'>newsTools</div>
+                        
+
+        </div>
+    );
+}
+
+export default Body;
