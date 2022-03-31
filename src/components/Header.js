abaee572d1dc687551845df4747b6a01eeb0ce83
Temir <31662@iitu.edu.kz>
2022-03-31 23:38:24 +0600

    Send Haruhi web TEST01
    тупо перенес лабки 8 Темира (то бишь моего) в папку репозитория

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import './Header.css'
+
+const Header = () =>{
+    return (
+        
+        <div className='Header'>
+            <div className='logo'></div>
+            <div className='headImg'> <h3> God Knows... </h3> </div>
+
+
+            <div className='ddMenu1'>メニュー</div>
+            <div className='ddMenu2'>歴史</div>
+            <div className='ddMenu3'>フォト</div>
+            <div className='ddMenu4'>ビデオ</div>
+            <div className='ddMenu5'>秘密</div>
+            <div className='ddMenu6'>について</div>
+
+            <div className='icon1'>Y</div>
+            <div className='icon2'>K</div>
+            <div className='icon3'>I</div>
+        </div>
+    );
+}
+
+export default Header;
