899a5b61864a4f0bcf0ca6edab69ee834e664045
Temir <31662@iitu.edu.kz>
2022-04-01 20:32:53 +0600

    111

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,11 @@ import './Footer.css'
 
 const Footer = () =>{
     return (
-<<<<<<< HEAD
         <footer className='Footer'>Footer
             
         </footer>
-=======
-        <div className='Footer'>Footer
-            
-        </div>
->>>>>>> a21db0d7fa59c362b3d3fb04f8bd6c82e78d1dad
+
+
     );
 }
 
