12980293abab70e9ec1cd8fa58cb18e6c74b60c5
BJIACTb <50963370+BJIACTb@users.noreply.github.com>
2022-04-01 22:09:11 +0600

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import './Footer.css'
 
 const Footer = () =>{
     return (
-        <div className='Footer'>Footerrrrrrrrrrrr
+        <div className='Footer'>Footerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
             
         </div>
 
